dde4f49e8fa83832178e6c8b3d5e794be554ae8e
JonathanRL <jonathan16@mhs.if.its.ac.id>
2017-11-15 07:46:38 +0700

    add readdir and read function

--- soal1.c
+++ soal1.c
@@ -12,7 +12,7 @@ static const char *dirpath = "/home/hehe/Documents";
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
-  int res;
+  	int res;
 	char fpath[1000];
 	sprintf(fpath,"%s%s",dirpath,path);
 	res = lstat(fpath, stbuf);
@@ -21,6 +21,62 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 		return -errno;
 
 	return 0;
+}
+
+static int xmp_readdir(const char *path,void *buf,fuse_fill_dir_t filler, off_t offset,struct fuse_file_info *fi)
+{
+	char fpath[1000];
+	if(strcmp(path,"/")==0){
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath,"%s%s",dirpath,path);
+
+	int res=0;
+
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(fpath);
+	if(dp == NULL) return -errno;
+
+	while((de = readdir(dp)) != NULL){
+		struct stat st;
+		memset(&st,0,sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		res = (filler(buf,de->d_name,&st,0));
+		if(res!=0)break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+static int xmp_read(const char *path,char *buf, size_t size,off_t offset, struct fuse_file_info *fi)
+{
+	char fpath[1000];
+	if(strcmp(path,"/")==0){
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath,"%s%s",dirpath,path);
+
+	int res=0,fd=0;
+
+	void(fi);
+
+	fd = open(fpath,O_RDONLY);
+	if(fd==-1)return -errno;
+
+	res = pread(fd,buf,size,offset);
+	if(res == -1)res = -errno;
+
+	close(fd);
+	return res;
 }
 
 static struct fuse_operations xmp_oper = {
